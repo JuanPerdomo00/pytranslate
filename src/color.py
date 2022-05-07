8ee53daa86dcbc8cbce3eaa558c7fef976d7d2ad
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-05-07 09:39:23 -0500

    class colors

---
+++ src/color.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Jakepy Perdomo <j4kyjak3@protonmail.com>. 
+
+from colorama import Fore
+
+class Color:
+    off: str = Fore.RESET
+    rojo: str = Fore.RED
+    azul: str = Fore.BLUE
+    verde: str = Fore.GREEN
+    amarillo: str = Fore.YELLOW
